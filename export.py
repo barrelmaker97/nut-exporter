030b4c1b5eb3a0b74bae17bfacf077b029c0b23d
Nolan Cooper <nolancooper97@gmail.com>
2020-11-11 17:30:54 -0600

    Move UPS Status to an Enum

--- export.py
+++ export.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-from prometheus_client import start_http_server, Gauge, Enum, Info
+from prometheus_client import start_http_server, Gauge, Enum
 import os
 import subprocess
 import time
+
+status = ["OL", "OB", "LB", "RB", "CHRG", "DISCHRG", "ALARM", "OVER", "TRIM", "BOOST", "BYPASS", "OFF", "CAL", "TEST", "FSD"]
+beeper_status = ["enabled", "disabled", "muted"]
 
 # Create metrics
 battery_charge = Gauge("ups_battery_charge", "Current Battery Charge")
@@ -15,12 +18,12 @@ battery_voltage_nominal = Gauge("ups_battery_voltage_nominal", "Nominal Battery 
 input_voltage = Gauge("ups_input_voltage", "Input Voltage")
 input_voltage_nominal = Gauge("ups_input_voltage_nominal", "Nominal Input Voltage")
 output_voltage = Gauge("ups_output_voltage", "Output Voltage")
-ups_beeper_status = Enum("ups_beeper_status", "Beeper Status", states=["enabled", "disabled", "muted"])
+ups_beeper_status = Enum("ups_beeper_status", "Beeper Status", states=beeper_status)
 ups_delay_shutdown = Gauge("ups_delay_shutdown", "Shutdown Delay")
 ups_delay_start = Gauge("ups_delay_start", "Start Delay")
 ups_load = Gauge("ups_load", "Load Percentage")
 ups_realpower_nominal = Gauge("ups_realpower_nominal", "Nominal Real Power")
-ups_status = Info("ups_status", "Status Code")
+ups_status = Enum("ups_status", "UPS Status Code", states=status)
 ups_timer_shutdown = Gauge("ups_timer_shutdown", "Shutdown Timer")
 ups_timer_start = Gauge("ups_timer_start", "Start Timer")
 
@@ -43,7 +46,7 @@ def check_stats(ups_name, ups_host, ups_port, poll_rate):
     ups_delay_start.set(clean_data.get("ups.delay.start"))
     ups_load.set(clean_data.get("ups.load"))
     ups_realpower_nominal.set(clean_data.get("ups.realpower.nominal"))
-    ups_status.info({"status": clean_data.get("ups.status")})
+    ups_status.state(clean_data.get("ups.status"))
     ups_timer_shutdown.set(clean_data.get("ups.timer.shutdown"))
     ups_timer_start.set(clean_data.get("ups.timer.start"))
     time.sleep(poll_rate)
